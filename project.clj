7a7170cda6203f2c50095fb0c86b7a15b2678b01
EricGebhart <e.a.gebhart@gmail.com>
2014-02-14 14:00:14 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,33 @@
+(defproject pail-schema "0.1.0-SNAPSHOT"
+  :description "Library for using Prismatic Schema, Pail and Cascalog."
+  :url "http://github.com/EricGebhart/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.0.0"
+
+  :source-paths ["src"]
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.backtype/dfs-datastores "1.3.4"]
+                 [org.apache.hadoop/hadoop-core "1.2.0" ]
+                 [potemkin "0.3.4"]
+                 [cascalog "2.0.0" ]
+                 [clj-pail-tap "0.1.0-SNAPSHOT"]
+                 [pail-fressian "0.1.1-SNAPSHOT"]
+                 [prismatic/schema "0.2.0"]
+                 [pail-cascalog "0.1.0"]]
+
+  :aot [pail-schema.data-unit-pail-structure]
+
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+
+             :dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.1"]]}}
+
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]
+                        ["snapshots" {:url "https://clojars.org/repo" :username :gpg :password :gpg}]])
---
+++ src/pail_schema/core.clj
@@ -0,0 +1,84 @@
+(ns pail-schema.core
+  "Defines Pail-Schema core functionality.
+   clj-pail-tap.core and pail-cascalog.core are integrated
+   into this namespace."
+  (:require [potemkin :as pt]
+            [schema.core :as s]
+            [clj-pail-tap.core]
+            [pail-cascalog.core])
+  (:import (com.backtype.hadoop.pail Pail PailSpec PailStructure)))
+
+; pull in clj-pail and pail-cascalog core functionality.
+(pt/import-vars [clj-pail-tap.core
+                    list-taps
+                    tap-map
+                    object-seq
+                    spec
+                    pail
+                    with-snapshot
+                    create
+                    find-or-create
+                    write-objects]
+                [pail-cascalog.core
+                    pail->tap
+                    tap-options
+                    tap])
+
+(defn get-tap
+  "Creates a `PailTap` from an existing vertically partitioned pail, by selecting an
+   entry from the Pail's tap map. Takes a pail connection. returns nil if no tap found."
+  [pail tap-key]
+  (when-let [attrs (tap-key (tap-map pail))]
+    (pail->tap pail :field-name (name tap-key)
+               :attributes [attrs])))
+
+; The rest is a descent parser to get property paths from a schema.
+; This is not complete, but it works for simple schemas.
+
+(declare get-field-keys)
+
+(defn- keys-for-either [schema]
+  (mapcat get-field-keys (:schemas schema)))
+
+(defn- key-keyword [keyish]
+  (condp = (type keyish)
+    schema.core.OptionalKey (:k keyish)
+    keyish))
+
+(defn- keys-for-map [schema]
+  (map vector
+       (map key-keyword (keys schema))
+       (map get-field-keys (vals schema))))
+
+(defn- get-field-keys [schema]
+  (condp = (type schema)
+    schema.core.Either (keys-for-either schema)
+    clojure.lang.PersistentArrayMap (keys-for-map schema)
+    []))
+
+
+(defn- node? [t]
+  (and (keyword? (first t))
+       (= 2 (count t))))
+
+(defn- last-node? [t]
+  (and (node? t)
+       (= [] (last t))))
+
+(defn- get-property-paths [tree]
+  (cond
+   (last-node? tree) [ [(first tree)] ]
+   (node? tree)
+   (let [children (get-property-paths (last tree))]
+     (map #(conj % (first tree)) children))
+   (seq? tree)
+     (mapcat get-property-paths tree)))
+
+
+(defn property-paths
+  "Get a list of property paths for a thrift data type.
+   Each row consists of a set of field maps leading
+   to a field, such that a path can be created for a property similar
+   to the way a Pail Partitioner does. Using field ids or field names."
+  [schema]
+  (map reverse (get-property-paths (get-field-keys schema))))
